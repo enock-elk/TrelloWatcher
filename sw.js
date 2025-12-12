b5526ae96ed24f3fcb3b4e245371a5164c149680
Enock Leo Kazembe <145878529+enock-elk@users.noreply.github.com>
2025-12-12 11:48:52 +0200

    Increase list limit to 5 and update SW

--- sw.js
+++ sw.js
@@ -1,23 +1,31 @@
-const CACHE_NAME = 'trello-watcher-v8'; // Updated to v8 to force immediate update
+const CACHE_NAME = 'trello-watcher-v10'; // Bumped to v10 to force update
 const urlsToCache = [
   './index.html',
   './manifest.json',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  // CACHE THE SOUNDS so they work offline/instantly
+  './sounds/bottle_opener.mp3',
+  './sounds/carlock.mp3',
+  './sounds/doorbell.mp3',
+  './sounds/email_notification.mp3',
+  './sounds/message_messaaaaaage.mp3',
+  './sounds/message_my_lord.mp3',
+  './sounds/message_tone.mp3'
 ];
 
 self.addEventListener('install', event => {
-  // Force this new service worker to become active immediately
-  self.skipWaiting();
-  
+  self.skipWaiting(); // Force active immediately
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('activate', event => {
-  // aggressive cleanup: remove ANY cache that doesn't match the current version
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -35,6 +43,12 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
