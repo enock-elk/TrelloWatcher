8da259da2e72047324262b162d000ed9bec1b826
Enock <enockelk@gmail.com>
2025-12-12 07:30:17 +0200

    Update: Added login feature

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'trello-watcher-v8'; // Updated to v8 to force immediate update
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  // Force this new service worker to become active immediately
+  self.skipWaiting();
+  
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  // aggressive cleanup: remove ANY cache that doesn't match the current version
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
